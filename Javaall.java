a0b543683627661afc97ae69559296ab843cbd2b
Diganta-academy <118277368+Diganta-academy@users.noreply.github.com>
2022-11-16 13:59:59 +0530

    Create Javaall.java

---
+++ Javaall.java
@@ -0,0 +1,24 @@
+//first program 
+public class main
+{
+  public static void main(String[] args)
+  {
+     System.out.println("Hello World");
+  }
+}
+
+
+//2 
+public class Main
+{
+   public static void main(String[] args)
+   {
+      String name="Diganta";
+      int age=22
+      String neighbour="Monotosh";
+      String friend= neighbour;
+     System.out.println("The name is= "+name);
+      System.out.println("The age "+age);
+      System.out.println("The friend is="+ friend);
+   }
+}
