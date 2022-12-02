1b86fbcb8303691ef84672a94b1083b17b1ec014
Diganta-academy <118277368+Diganta-academy@users.noreply.github.com>
2022-12-02 16:31:27 +0530

    Delete r programming 1.R

--- r programming 1.R
+++
@@ -1,7 +0,0 @@
-name<-c("Saswata da","Dipanjan","sangita di","Babai da","Papai da","Chayanika di","Mriganko vai","Diganta")
-age<-c(21,26,23,26,32,27,24,22)
-colors<-c("blue","yellow","green","black","red","white")
-pie(age,label=name,main="Family",col =colors)
-
-#Display the Explanation box
-legend("bottomright",name,fill=colors)
